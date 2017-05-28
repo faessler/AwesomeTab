d9c89d98a18771d0bad04645e3b4bf27f476a000
Jan Fässler <jan@faessler.be>
2017-05-28 16:11:52 +0200

    init hometab extension

---
+++ newtab.js
@@ -0,0 +1,42 @@
+// Stuff
+
+console.log("test");
+
+var userName = 'Jan';
+
+// TIME
+function startTime() {
+	var today = new Date();
+	var h = today.getHours();
+	var m = today.getMinutes();
+	var s = today.getSeconds();
+	m = checkTime(m);
+	s = checkTime(s);
+	document.getElementById('time').innerHTML =
+	h + ":" + m;
+	// h + ":" + m + ":" + s;
+	var t = setTimeout(startTime, 500);
+
+	if (h < 12) {
+		document.getElementById('hello').innerHTML = 'good morning, ' + userName;
+		// document.body.style.backgroundImage = "url('https://source.unsplash.com/2560x1440/daily?landscape,morning')";
+	} else if (h < 18) {
+		document.getElementById('hello').innerHTML = 'good afternoon, ' + userName;
+		// document.body.style.backgroundImage = "url('https://source.unsplash.com/2560x1440/daily?landscape,day')";
+	} else {
+		document.getElementById('hello').innerHTML = 'good evening, ' + userName;
+		document.body.style.backgroundImage = "url('https://source.unsplash.com/2560x1440/daily?landscape,night')";
+	}
+}
+function checkTime(i) {
+	if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+	return i;
+}
+document.onreadystatechange = () => {
+	if (document.readyState === 'complete') {
+		startTime()
+	}
+};
+
+
+// document.body.style.backgroundImage="url('https://images.unsplash.com/photo-1483976503408-d7e149fbf0d3?dpr=2&auto=format&fit=crop&w=1500&h=1001&q=80&cs=tinysrgb&crop=&bg=')";
